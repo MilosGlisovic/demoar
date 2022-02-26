e0ca5a70837d9ad3bad77dcaeaccb354c63ec316
Milos Glisovic <60900969+MilosGlisovic@users.noreply.github.com>
2022-02-26 16:06:15 +0100

    Add files via upload

---
+++ places.js
@@ -0,0 +1,1189 @@
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+   return [
+
+    
+     
+
+    {
+     name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+     logo: 's.png',
+     width: "1",
+     height: "0.8",
+     location: {
+         lat: 51.50845, 
+         lng: -0.12884,
+     }
+  
+ },
+  
+
+   
+ {
+     name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+     logo: 's4.png',
+     width: "0.8",
+     height: "1",
+     location: {
+         lat: 51.50836,
+         lng: -0.12867,
+     }
+ },
+ 
+
+ {
+     name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+     logo: 's2.png',
+     width: "1",
+     height: "0.8",
+     location: {
+         lat: 51.508462,
+         lng: -0.128573,
+     }
+ },
+ 
+
+ {
+     name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+     logo: 's3.png',
+     width: "0.8",
+     height: "1",
+     location: {
+         lat: 51.50838,
+         lng: -0.12838,
+     }
+ },
+ 
+
+
+
+
+
+
+ {
+    name: 'Ivana Knezevic - FAMMILY',
+    logo: '7.png',
+    width: "1",
+    height: "0.6",
+    location: {
+        lat:  51.50853,
+        lng: -0.12829,
+    }
+ 
+},
+
+
+
+{
+    name: 'Ivana Knezevic - CITY',
+    logo: '6.png',
+    width: "1",
+    height: "1",
+    location: {
+        lat: 51.50844, 
+        lng: -0.12818,
+    }
+},
+
+
+
+{
+    name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+    logo: '5.png',
+    width: "1",
+    height: "0.9",
+    location: {
+        lat: 51.50854,
+        lng: -0.12804,
+    }
+},
+
+
+
+{
+    name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+    logo: '4.png',
+    width: "1",
+    height: "1",
+    location: {
+        lat: 51.50846,
+        lng: -0.12790,
+    }
+},
+
+{
+    name: 'Jean-Marie Guayux - FRIDA',
+    logo: '9s.png',
+    width: "1",
+    height: "1",
+    location: {
+        lat: 51.50854,
+        lng: -0.12763,
+    }
+},
+
+{
+    name: 'Jean-Marie Guayux - WONDER WOMAN',
+    logo: 'WonderWoman.png',
+    width: "1.",
+    height: "1",
+    location: {
+        lat: 45.64763, 
+        lng: -0.15360,
+    }
+ 
+},
+
+  
+
+
+
+{
+    name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 40.78095, 
+        lng: -73.96463,
+    }
+ 
+},
+ 
+
+
+  
+{
+    name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+    logo: 's4.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 40.78107,
+        lng: -73.96454,
+    }
+},
+
+, 
+
+{
+    name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's2.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 40.78101,
+        lng: -73.96440,
+    }
+},
+
+
+{
+    name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+    logo: 's3.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 51.50838,
+        lng: -0.12838,
+    }
+},
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - FAMMILY',
+   logo: '7.png',
+   width: "1",
+   height: "0.6",
+   location: {
+       lat:  40.78109,
+       lng: -73.96424,
+   }
+
+},
+
+
+
+
+{
+   name: 'Ivana Knezevic - CITY',
+   logo: '6.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.78144, 
+       lng: -73.96429,
+   }
+},
+
+
+
+
+{
+   name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+   logo: '5.png',
+   width: "1",
+   height: "0.9",
+   location: {
+       lat: 40.78136,
+       lng: -73.96439,
+   }
+},
+
+
+
+{
+   name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+   logo: '4.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.78124,
+       lng: -73.96443,
+   }
+},
+
+
+{
+   name: 'Jean-Marie Guayux - FRIDA',
+   logo: '9s.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.78122,
+       lng: -73.96462,
+   }
+},
+
+
+{
+   name: 'Jean-Marie Guayux - WONDER WOMAN',
+   logo: 'WonderWoman.png',
+   width: "1.",
+   height: "1",
+   location: {
+       lat: 40.78110, 
+       lng: -73.96473,
+   }
+
+},
+
+
+
+
+{
+    name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 40.74559, 
+        lng: -73.94769,
+    }
+ 
+},
+ 
+
+
+
+  
+{
+    name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+    logo: 's4.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 40.74557,
+        lng: -73.94751,
+    }
+},
+
+
+
+
+{
+    name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's2.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 40.74546,
+        lng: -73.94739,
+    }
+},
+
+
+{
+    name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+    logo: 's3.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 40.74553,
+        lng: -73.94727,
+    }
+},
+
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - FAMMILY',
+   logo: '7.png',
+   width: "1",
+   height: "0.6",
+   location: {
+       lat:  40.74541,
+       lng: -73.94727,
+   }
+
+},
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - CITY',
+   logo: '6.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.74550, 
+       lng: -73.94713,
+   }
+},
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+   logo: '5.png',
+   width: "1",
+   height: "0.9",
+   location: {
+       lat: 40.74556,
+       lng: -73.94699,
+   }
+},
+
+
+
+
+{
+   name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+   logo: '4.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.74560,
+       lng: -73.94714,
+   }
+},
+
+
+
+{
+   name: 'Jean-Marie Guayux - FRIDA',
+   logo: '9s.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.74565,
+       lng: -73.94698,
+   }
+},
+
+
+
+{
+   name: 'Jean-Marie Guayux - WONDER WOMAN',
+   logo: 'WonderWoman.png',
+   width: "1.",
+   height: "1",
+   location: {
+       lat: 40.74568, 
+       lng: -73.94722,
+   }
+
+},
+
+
+
+
+{
+    name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 40.67114, 
+        lng: -73.96222,
+    }
+ 
+},
+ 
+
+ 
+
+
+  
+{
+    name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+    logo: 's4.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 40.67111,
+        lng: -73.96204,
+    }
+},
+
+
+
+
+
+{
+    name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's2.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 40.67099,
+        lng: -73.96207,
+    }
+},
+
+
+{
+    name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+    logo: 's3.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 40.74553,
+        lng: -73.94727,
+    }
+},
+
+
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - FAMMILY',
+   logo: '7.png',
+   width: "1",
+   height: "0.6",
+   location: {
+       lat:  40.67098,
+       lng: -73.96190,
+   }
+
+},
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - CITY',
+   logo: '6.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.670877, 
+       lng: -73.96176,
+}
+
+},
+
+
+
+{
+    name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+    logo: '5.png',
+    width: "1",
+    height: "0.9",
+    location: {
+        lat: 40.670735,
+        lng: -73.961862,
+    }
+ },
+
+
+
+ 
+
+
+{
+   name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+   logo: '4.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.670619,
+       lng: -73.96200,
+   }
+},
+
+
+
+
+{
+   name: 'Jean-Marie Guayux - FRIDA',
+   logo: '9s.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 40.670731,
+       lng: -73.962117,
+   }
+},
+
+
+
+
+{
+   name: 'Jean-Marie Guayux - WONDER WOMAN',
+   logo: 'WonderWoman.png',
+   width: "1.",
+   height: "1",
+   location: {
+       lat: 40.670872, 
+       lng: -73.962195,
+   }
+
+},
+
+
+
+
+{
+    name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 50.841232, 
+        lng: 4.390437,
+    }
+ 
+},
+ 
+
+ 
+
+
+
+  
+{
+    name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+    logo: 's4.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 50.84130,
+        lng: 4.39026,
+    }
+},
+
+
+
+
+
+
+{
+    name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's2.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 50.841297,
+        lng: 4.390077,
+    }
+},
+
+
+
+{
+    name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+    logo: 's3.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 50.841390,
+        lng: 4.389932,
+    }
+},
+
+
+
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - FAMMILY',
+   logo: '7.png',
+   width: "1",
+   height: "0.6",
+   location: {
+       lat:  50.84142,
+       lng: 4.39011,
+   }
+
+},
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - CITY',
+   logo: '6.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 50.84151, 
+       lng: 4.39024,
+}
+
+},
+
+
+
+
+{
+    name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+    logo: '5.png',
+    width: "1",
+    height: "0.9",
+    location: {
+        lat: 50.84152,
+        lng: 4.39044,
+    }
+ },
+
+
+
+ 
+
+
+
+{
+   name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+   logo: '4.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 50.84145,
+       lng: 4.390575,
+   }
+},
+
+
+, 
+
+
+{
+   name: 'Jean-Marie Guayux - FRIDA',
+   logo: '9s.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 50.84133,
+       lng: 4.39061,
+   }
+},
+
+
+
+
+
+{
+   name: 'Jean-Marie Guayux - WONDER WOMAN',
+   logo: 'WonderWoman.png',
+   width: "1.",
+   height: "1",
+   location: {
+       lat: 50.84140, 
+       lng: 4.39036,
+   }
+
+},
+
+
+
+
+{
+    name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 50.84391, 
+        lng: 4.36330,
+    }
+ 
+},
+ 
+
+ 
+ 
+
+
+  
+{
+    name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+    logo: 's4.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 50.843825,
+        lng: 4.363472,
+    }
+},
+
+
+
+
+
+
+{
+    name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's2.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 50.841297,
+        lng: 4.390077,
+    }
+},
+
+, 
+
+
+{
+    name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+    logo: 's3.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 50.84370,
+        lng: 4.36352,
+    }
+},
+
+
+
+
+
+
+
+, 
+
+
+
+
+{
+   name: 'Ivana Knezevic - FAMMILY',
+   logo: '7.png',
+   width: "1",
+   height: "0.6",
+   location: {
+       lat:  50.84356,
+       lng: 4.36346,
+   }
+
+},
+
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - CITY',
+   logo: '6.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 50.84347, 
+       lng: 4.36325,
+}
+
+},
+
+
+
+
+{
+    name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+    logo: '5.png',
+    width: "1",
+    height: "0.9",
+    location: {
+        lat: 50.84152,
+        lng: 4.39044,
+    }
+ },
+
+
+ 
+
+ 
+
+
+
+
+{
+   name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+   logo: '4.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 50.84357,
+       lng: 4.36284,
+   }
+},
+
+
+
+
+
+{
+   name: 'Jean-Marie Guayux - FRIDA',
+   logo: '9s.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 50.84133,
+       lng: 4.39061,
+   }
+},
+
+
+
+
+
+
+{
+   name: 'Jean-Marie Guayux - WONDER WOMAN',
+   logo: 'WonderWoman.png',
+   width: "1.",
+   height: "1",
+   location: {
+       lat: 50.84370, 
+       lng: 4.36276,
+   }
+
+},
+
+
+{
+    name: 'Sandra Vucicevic - CITY AT SPEED - 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 41.91507, 
+        lng: 12.48251,
+    }
+ 
+},
+ 
+
+ 
+ 
+
+
+
+  
+{
+    name: 'Sandra Vucicevic - HERE COMES THE SUN - 2020, Acrylic on canvas, 16x20 inch',
+    logo: 's4.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 41.91519,
+        lng: 12.48262,
+    }
+},
+
+
+
+
+
+
+
+{
+    name: 'Sandra Vucicevic - PLEASANTVILLE – 2019, Acrylic on canvas, 20x16 inch',
+    logo: 's2.png',
+    width: "1",
+    height: "0.8",
+    location: {
+        lat: 41.91527,
+        lng: 12.48276,
+    }
+},
+ 
+
+
+
+{
+    name: 'Sandra Vucicevic - WIND OF CHANGE – 2019, Acrylic on canvas, 16x20 inch',
+    logo: 's3.png',
+    width: "0.8",
+    height: "1",
+    location: {
+        lat: 41.91531,
+        lng: 12.48292,
+    }
+},
+
+
+
+
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - FAMMILY',
+   logo: '7.png',
+   width: "1",
+   height: "0.6",
+   location: {
+       lat:  41.91531,
+       lng: 12.48310,
+   }
+
+},
+
+
+
+
+
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - CITY',
+   logo: '6.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 41.91524, 
+       lng: 12.48328,
+}
+
+},
+
+
+
+
+
+{
+    name: 'Ivana Knezevic - GALAKSY FIREWORKS',
+    logo: '5.png',
+    width: "1",
+    height: "0.9",
+    location: {
+        lat: 41.91524,
+        lng: 12.48347,
+    }
+ },
+
+
+ 
+
+ 
+
+
+
+
+
+{
+   name: 'Ivana Knezevic - DARK WOOD 15x15x15cm',
+   logo: '4.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 41.91513,
+       lng: 12.48356,
+   }
+},
+
+
+
+ 
+
+
+{
+   name: 'Jean-Marie Guayux - FRIDA',
+   logo: '9s.png',
+   width: "1",
+   height: "1",
+   location: {
+       lat: 41.91499,
+       lng: 12.48354,
+   }
+},
+
+
+
+
+
+
+{
+   name: 'Jean-Marie Guayux - WONDER WOMAN',
+   logo: 'WonderWoman.png',
+   width: "1.",
+   height: "1",
+   location: {
+       lat: 41.91486, 
+       lng: 41.91486,
+   }
+
+},
+
+
+   ];
+}
+
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+       let name2 = place.name;
+       let logo = place.logo;
+       let wdt = place.width;
+       let hg = place.height;
+
+
+       
+
+       const model = document.createElement('a-image');
+                   model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+                   model.setAttribute('name', name2);
+                   model.setAttribute('src', logo);
+                   model.setAttribute('width', wdt); 
+                   model.setAttribute('height', hg); 
+                   model.setAttribute('look-at', '[gps-camera]');
+
+
+
+
+
+                   // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+                   model.setAttribute('scale', '14, 14');
+
+                   
+
+    
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       
+
+
+       
+       const clickListener = function(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        const name = ev.target.getAttribute('name');
+        
+        const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+        //const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+
+
+        if (el && el === ev.target) {
+
+    
+            const label = document.createElement('span');
+            //const msg = document.createElement('p');
+            const container = document.createElement('div');
+            const btn = document.createElement('button');
+            //const link = document.createElement('a');
+           // const btn1 = document.createElement('button');
+            container.setAttribute('id', 'place-label');
+            //link.setAttribute('href', place.link2);
+            label.innerText = name;
+           //msg.innerText = distanceMsg;
+            btn.innerText = 'Close';
+            //link.innerText = 'Zavrtite tocak';
+           // btn1.innerText = 'Play';
+            container.appendChild(label);
+            //container.appendChild(msg);
+            container.appendChild(btn);
+            //container.appendChild(link);
+            //container.appendChild(btn1);
+            document.body.appendChild(container);
+
+           // btn1.addEventListener("click", function() {
+             //   let play = new SpeechSynthesisUtterance();
+              // play.text = name;
+              // window.speechSynthesis.speak(play);
+              //});
+
+            btn.addEventListener("click", function() {
+                container.parentElement.removeChild(container);
+              });
+
+           // setTimeout(() => {
+             //   container.parentElement.removeChild(container);
+            //}, 1500);
+        }
+    };
+
+    model.addEventListener('click', clickListener);
+       scene.appendChild(model);
+   });
+}
